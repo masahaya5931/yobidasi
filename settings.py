4c559207afa07f95adfaf57c08b6ccd71ca4bce1
cyber-yuito723 <cyber.yuito723@icloud.com>
2023-10-15 14:53:12 +0900

    Update run.pyw

--- settings.py
+++ settings.py
@@ -2,21 +2,6 @@
 TeaChimer-v1.0.0
 (C) 2023 cyber-yuito723
 
-
-This file is part of TeaChimer.
-
-TeaChimer is free software: you can redistribute it and/or modify it under the terms of
-the GNU General Public License as published by the Free Software Foundation,
-either version 3 of the License, or (at your option) any later version.
-
-TeaChimer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with TeaChimer.
-If not, see <https://www.gnu.org/licenses/>.
-
-
 school_name = "school name"
 admin_name = "admin's name"
 main_tab_*_n = "tab string"
