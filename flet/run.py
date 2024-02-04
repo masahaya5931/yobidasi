c4c76466bf11fed4b73633a7035d2c581fe842c3
cyber-yuito723 <129262838+cyber-yuito723@users.noreply.github.com>
2024-02-04 09:49:52 +0000

    20240204

--- flet/run.py
+++ flet/run.py
@@ -1,5 +1,19 @@
 """
 TeaChimer-v2.0.0
-(C) 2024 cyber-yuito723
+(C) 2023-2024 cyber-yuito723
 """
 
+import sys
+sys.dont_write_bytecode = True
+
+import flet as ft
+import time
+
+with open("./starting.txt", "r", encoding = "utf-8") as f:
+    starting_txt = f.read()
+print(starting_txt)
+
+
+time.sleep(3)
+
+port = 23080
